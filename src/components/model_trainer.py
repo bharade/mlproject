f5a52398d21a492990941df660524e285a073798
bharade <adibharade@gmail.com>
2024-10-18 20:52:41 +0530

    prediction pipelin

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -109,11 +109,6 @@ class ModelTrainer:
             predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
-            return r2_square
-            
-
-
-
-            
+            return r2_square      
         except Exception as e:
             raise CustomException(e,sys)
